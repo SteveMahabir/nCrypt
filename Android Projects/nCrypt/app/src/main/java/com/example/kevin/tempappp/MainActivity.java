002d51b5e4c684cc92b589f350b1789cbb1845ab
katrinabosch <katrinajbosch@gmail.com>
2015-09-25 14:12:52 -0400

    added sms methods (not tested or hooked up yet)

--- Android Projects/nCrypt/app/src/main/java/com/example/kevin/tempappp/MainActivity.java
+++ Android Projects/nCrypt/app/src/main/java/com/example/kevin/tempappp/MainActivity.java
@@ -1,19 +1,57 @@
 package com.example.kevin.tempappp;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    private String phoneNumber;
+    private EditText edtMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtMessage=(EditText)findViewById(R.id.chatLine);
+    }
+
+    public void sendMsg() {
+
+
+        String sent = "SMS_SENT";
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(sent), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                if(getResultCode() == Activity.RESULT_OK)
+                {
+                    Toast.makeText(getBaseContext(), "SMS sent",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getBaseContext(), "SMS could not sent",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new IntentFilter(sent));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, edtMessage.getText().toString(), sentPI, null);
     }
 
     @Override
---
+++ Android Projects/nCrypt/app/src/main/java/com/example/kevin/tempappp/MessageReceiver.java
@@ -0,0 +1,33 @@
+package com.example.kevin.tempappp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class MessageReceiver extends BroadcastReceiver {
+    public MessageReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] recievedMsgs = null;
+        String str = "";
+        if (bundle != null)
+        {
+
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            recievedMsgs = new SmsMessage[pdus.length];
+            for (int i=0; i < pdus.length; ++i)
+            {
+                recievedMsgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                str += "SMS from " + recievedMsgs[i].getOriginatingAddress()+ " :" + recievedMsgs[i].getMessageBody().toString();
+            }
+
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
