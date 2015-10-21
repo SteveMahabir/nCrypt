c5899cc96a01fd9b5d0a3f7bddbccced173389f1
mahmadij <mahmadij@gmail.com>
2015-10-20 21:39:29 -0400

    Added files

---
+++ Android Projects/nCrypt/app/src/main/java/com/example/kevin/tempappp/NotificationReceiver.java
@@ -0,0 +1,12 @@
+package com.example.kevin.tempappp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationReceiver extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
